fccb4ebe3096cf568af5b76b6f88a1d38fa998e4
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-02-08 13:50:12 +0500

    Login add

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./store";
-import { Provider } from "react-redux";
+import { AuthProvider } from "./context/Auth";
+// import { store } from "./store";
+// import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  // <Provider store={store}>
+  <AuthProvider > 
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </AuthProvider>
+  // </Provider>
 );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,47 +2,135 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { NavLink } from "react-router-dom";
+import { useState, useRef, useEffect, useContext } from "react";
+import AuthContext from "../../context/Auth";
+import { createContext } from "react";
 import "./style.scss";
+import axios from "../../api/axios";
+
+const LOGIN_URL = "/auth";
+
 const index = () => {
+  const { setAuth } = useContext(AuthContext);
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      // console.log(user, pwd);
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch(err) {
+      if (!err?.response) {
+        setErrMsg("No server response");
+      } else if (err.response?.status === 400) {
+      setErrMsg("Missing username or password")
+      }
+      else if(err.response?.status===401){
+    setErrMsg('UnAutorization')
+      }else{
+        setErrMsg("login failed")
+      }
+      errRef.current.focus()
+    }
+  };
+
   return (
     <>
-      <div className="d-flex py-4 flex-column justify-content-center align-items-center">
-        <div className="login-top d-flex flex-column aling-items-center w-50 justify-content-center">
-          <h1 className="text-warning text-center">Log in </h1>
-          <p className="text-center my-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-            necessitatibus suscipit molestiae architecto expedita omnis, in
-            magnam iste rem, mollitia, nam non provident quisquam animi. Animi
-            rerum similique ratione natus!
+      {success ? (
+        <section>
+          <h2>Your are logged in!</h2>
+          <br />
+          <p>
+            <a href="/">Go to home</a>
           </p>
-        </div>
-        <Form className="form-admin w-50">
-          <Form.Group className="mb-3 w-100 fw-bold" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" required placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group
-            className="mb-3 w-100 fw-bold"
-            controlId="formBasicPassword"
+        </section>
+      ) : (
+        // }
+        <div className="d-flex py-4 flex-column justify-content-center align-items-center">
+          <div className="login-top d-flex flex-column aling-items-center w-50 justify-content-center">
+            <h1 className="text-warning text-center">Log in </h1>
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <p className="text-center my-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consectetur necessitatibus suscipit molestiae architecto expedita
+              omnis, in magnam iste rem, mollitia, nam non provident quisquam
+              animi. Animi rerum similique ratione natus!
+            </p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="form-admin d-flex justify-content-between align-items-start flex-column  w-50"
           >
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" required placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3 w-100 " controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" required label="Check me out" />
-          </Form.Group>
-          <Button variant="warning" className="w-25 btn-submit" type="submit">
-            Submit
-          </Button>
-          <NavLink to="/sign" className="sign-in btn btn-outline-warning mx-3">
-            Sign In
-          </NavLink>
-        </Form>
-      </div>
+            <label className="w-100 my-3">
+              Email address
+              <input
+                className="form-control w-100 mt-2"
+                type="text"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                autoComplete={"off"}
+                ref={userRef}
+                required
+                placeholder="Enter email"
+              />
+            </label>
+            <label className="label w-100 my-2">
+              Password
+              <input
+                className="form-control w-100 my-2"
+                type="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                placeholder="Password"
+              />
+            </label>
+            <div className="d-flex justify-content-start gap-2 align-items-center w-100">
+              <button className="w-25 btn btn-warning btn-submit" type="submit">
+                Submit
+              </button>
+              <NavLink to="/sign" className="sign-in btn btn-outline-warning">
+                Sign In
+              </NavLink>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
