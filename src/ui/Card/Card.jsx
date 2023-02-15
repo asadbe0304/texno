1265b36b4ff14462d7d0af240c3b40891b72c988
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-02-15 19:37:33 +0500

    ook

--- src/ui/Card/Card.jsx
+++ src/ui/Card/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({prop}) => {
       <div className="card__popular d-flex align-items-center gap-2 flex-column justify-content-between bg-dark p-2">
         <img src={prop.image} className="img-card position-absolute " alt="images" />
        <div className="d-flex justify-content-center px-4 align-items-center w-100 flex-row">
-        <h6 className="fw-bold text-white text-end m-0 my-2">Холодильники</h6>
+        <h6 className="fw-bold category-cap text-white text-end m-0 my-2">Холодильники</h6>
        </div>
       </div>
     </>
